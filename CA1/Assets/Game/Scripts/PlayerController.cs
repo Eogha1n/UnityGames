698cc8088decc15756c58306e0b66c43fc819427
Mystery987 <eoghain.mcgrath@gmail.com>
2018-11-19 21:29:10 +0000

    Started work on CA1 following tutorial.

---
+++ CA1/Assets/Game/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float jumpForce;
+    public CharacterController controller;
+
+    private Vector3 moveDirection;
+    public float gravityScale;
+
+	// Use this for initialization
+	void Start ()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0f, Input.GetAxis("Vertical") * moveSpeed);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            moveDirection.y = jumpForce;
+        }
+
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale);
+        controller.Move(moveDirection * Time.deltaTime);
+	}
+}
